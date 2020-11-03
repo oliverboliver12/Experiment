77beb2837de953383e214b63b4fab0077b512ba8
oliver <oliver>
2020-11-03 14:31:30 +0200

    Back to main

--- app/src/main/java/com/example/experiment/MainActivity.java
+++ app/src/main/java/com/example/experiment/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                 s       .setAction("Action", null).show();
             }
         });
     }
